de6d2f6c69852df66391bff83e49def45fe0aa7b
syoumar00000 <62055780+syoumar00000@users.noreply.github.com>
2021-03-22 23:03:57 +0000

    mon premier commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+import 'rss_reader.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // Changed my theme to look more like a terminal. Colors defined in
+        // colors.dart
+        brightness: Brightness.dark,
+        primaryColor: colorHackerBackground,
+        accentColor: colorHackerBorder,
+
+        // Defining the default font family.
+        fontFamily: 'roboto',
+
+        // Modifying the default TextTheme to help with my terminal design.
+        primaryTextTheme: TextTheme(
+          title: TextStyle(),
+        ).apply(
+            bodyColor: colorHackerHeading, displayColor: colorHackerHeading),
+      ),
+      home: RSSReader(),
+    );
+  }
+}
